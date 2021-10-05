0e8462fe5b977aa34836f19827877987c68c1c89
willjun0115 <willjun0115@gmail.com>
2021-10-05 20:10:18 +0900

    a

--- MySite/main/views.py
+++ MySite/main/views.py
@@ -11,7 +11,7 @@ def visitor_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
