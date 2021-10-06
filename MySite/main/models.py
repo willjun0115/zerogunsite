19242d18002b7c4034479cedb9efa804715fc536
willjun0115 <willjun0115@gmail.com>
2021-10-07 07:02:51 +0900

    a

--- MySite/main/models.py
+++ MySite/main/models.py
@@ -16,7 +16,7 @@ class User(models.Model):
     ip = models.GenericIPAddressField(protocol='IPv4', null=True)
     username = models.CharField(max_length=16, default='user')
     date = models.DateTimeField('date created', auto_now_add=True)
-    allowed_board_id = ArrayField(models.CharField(), blank=True, null=True)
+    allowed_board_id = ArrayField(models.CharField(max_length=10, blank=True), null=True)
 
     def __str__(self):
         return self.username
