858e35a1975ef0ac9da7b522ff0601e37d9d1f57
willjun0115 <willjun0115@gmail.com>
2021-10-07 07:21:56 +0900

    a

--- MySite/config/settings.py
+++ MySite/config/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DATABASE NAME',
-        'USER': 'USER NAME',
-        'PASSWORD': 'USER PASSWORD',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
