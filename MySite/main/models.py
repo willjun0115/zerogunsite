d8d31940da7aacc374b3781121e2a088dc323e64
willjun0115 <willjun0115@gmail.com>
2021-10-08 08:19:52 +0900

    a

--- MySite/main/models.py
+++ MySite/main/models.py
@@ -16,7 +16,7 @@ class User(models.Model):
     username = models.CharField(max_length=16, default='user')
     date = models.DateTimeField('date created', auto_now_add=True)
     allowed_board_id = models.CharField(max_length=100, blank=True, null=True)
-    liked_post_id = models.CharField(max_length=2000, blank=True, null=True)
+    liked_post_id = models.CommaSeparatedIntegerField(max_length=1000)
 
     def __str__(self):
         return self.username
--- MySite/main/views.py
+++ MySite/main/views.py
@@ -70,18 +70,18 @@ def like(request, post_id):
     user = get_user_or_create(request)
     post = get_object_or_404(Post, pk=post_id)
     if post.writer.ip != user.ip:
-        liked_list = user.liked_post_id.split()
-        if str(post.id) in liked_list:
+        liked_list = user.liked_post_id.split(',')
+        if post.id in liked_list:
             post.likes -= 1
             post.save()
-            liked_list.remove(str(post.id))
-            user.liked_post_id = ' '.join(liked_list)
+            liked_list.remove(post.id)
+            user.liked_post_id = ','.join(liked_list)
             user.save()
         else:
             post.likes += 1
             post.save()
-            liked_list.append(str(post.id))
-            user.liked_post_id = ' '.join(liked_list)
+            liked_list.append(post.id)
+            user.liked_post_id = ','.join(liked_list)
             user.save()
     return HttpResponseRedirect(reverse('main:board', args=(post.board.id,)))
 
